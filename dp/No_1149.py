d5f9013f322e4e97bafb88814cbd77859a30e750
Minsu Choi <mine52100@naver.com>
2024-12-29 14:53:07 +0900

    dp: 백준 1149번 RGB거리 문제 해결

--- dp/No_1149.py
+++ dp/No_1149.py
@@ -1,13 +1,18 @@
-N = int(input())
+def solution(n, n_list):
+    dp = [[], n_list[1]]
 
-costs = []
-for i in range(N):
-    rgb = list(map(int, input().split(" ")))
-    costs.append(rgb)
+    for i in range(2, n + 1):
+        dp.append([])
+        dp[i].append(min(dp[i-1][1], dp[i-1][2]) + n_list[i][0])
+        dp[i].append(min(dp[i-1][0], dp[i-1][2]) + n_list[i][1])
+        dp[i].append(min(dp[i-1][0], dp[i-1][1]) + n_list[i][2])
 
-for i in range(N-2, -1, -1):
-    costs[i][0] += min(costs[i+1][1], costs[i+1][2])
-    costs[i][1] += min(costs[i+1][0], costs[i+1][2])
-    costs[i][2] += min(costs[i+1][0], costs[i+1][1])
+    return min(dp[n][0], dp[n][1], dp[n][2])
 
-print(min(costs[0][0], costs[0][1], costs[0][2]))
+n = int(input())
+n_list = [[]]
+
+for i in range(n):
+    n_list.append(list(map(int, input().split(" "))))
+
+print(solution(n, n_list))
