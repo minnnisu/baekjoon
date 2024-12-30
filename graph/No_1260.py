f0171068314895c259e4aff0dfd74832da801ace
Minsu Choi <mine52100@naver.com>
2024-12-30 15:44:09 +0900

    graph: 백준 1260번 DFS와 BFS 문제 해결

--- graph/No_1260.py
+++ graph/No_1260.py
@@ -1,44 +1,44 @@
 from collections import deque
 
-def dfs(graph, start, visited):
+def dfs(now):
+    if(visited[now]):
+        return
+
+    visited[now] = True
+    print(now, end=" ")
+
+    for node in graph[now]:
+        dfs(node)
+
+def bfs(start):
+    queue = deque([start])
     visited[start] = True
-    print(start, end=" ")
 
-    for node in graph[start]:
-        if(visited[node] == False):
-            dfs(graph, node, visited)
+    while queue:
+        podded_node = queue.popleft()
+        print(podded_node, end=" ")
+        
+        for node in graph[podded_node]:
+            if(visited[node] == False):
+                queue.append(node)
+                visited[node] = True
 
-def bfs(graph, start, visited):
-    graph_deque = deque([start])
-    visited[start] = True
 
-    while len(graph_deque) > 0 :
-        node = graph_deque.popleft()
-        print(node, end=" ")
 
-        for i in graph[node]:
-            if(visited[i] == False):
-                visited[i] = True
-                graph_deque.append(i)
+n, m, v = map(int, input().split(" "))
 
-N, M, V = map(int, input().split(" "))
-
-graph = []
-visited_bfs = []
-visited_dfs = []
-for i in range(N + 1):
-    visited_dfs.append(False)
-    visited_bfs.append(False)
-    graph.append([])
-
-for i in range(M):
+graph = [[] for i in range(n + 1)]
+for i in range(m):
     node1, node2 = map(int, input().split(" "))
     graph[node1].append(node2)
     graph[node2].append(node1)
 
-for i in range(1, N + 1):
-    graph[i].sort()
+for i in graph:
+    i.sort()
 
-dfs(graph, V, visited_dfs)
+visited = [False for i in range(n + 1)]
+dfs(v)
 print()
-bfs(graph, V, visited_bfs)
+
+visited = [False for i in range(n + 1)]
+bfs(v)
